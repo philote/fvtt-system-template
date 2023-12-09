9c56b4683aa4627f0f60049127780b7e17db0558
Joseph Hopson <727468+philote@users.noreply.github.com>
2023-12-08 19:18:17 -0700

    adding more boilerplate files

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const FST = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ FST.abilities = {
+  "str": "FST.AbilityStr",
+  "dex": "FST.AbilityDex",
+  "con": "FST.AbilityCon",
+  "int": "FST.AbilityInt",
+  "wis": "FST.AbilityWis",
+  "cha": "FST.AbilityCha"
+};
+
+FST.abilityAbbreviations = {
+  "str": "FST.AbilityStrAbbr",
+  "dex": "FST.AbilityDexAbbr",
+  "con": "FST.AbilityConAbbr",
+  "int": "FST.AbilityIntAbbr",
+  "wis": "FST.AbilityWisAbbr",
+  "cha": "FST.AbilityChaAbbr"
+};
